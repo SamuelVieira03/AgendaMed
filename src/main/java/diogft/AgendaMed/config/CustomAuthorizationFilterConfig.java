70ad8dd3468533606d5345a5a680f43f9916336b
SamuelVieira03 <82007689+SamuelVieira03@users.noreply.github.com>
2023-09-10 18:12:03 -0300

    Solving connection endpoint

---
+++ src/main/java/diogft/AgendaMed/config/CustomAuthorizationFilterConfig.java
@@ -0,0 +1,58 @@
+package diogft.AgendaMed.config;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.util.Strings;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@RequiredArgsConstructor
+public class CustomAuthorizationFilterConfig extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authorizationHeader = request.getHeader("Authorization");
+
+        log.info("request sendo feito na aplicacao");
+
+        if (Strings.isNotEmpty(authorizationHeader) && authorizationHeader.startsWith("Bearer ")) {
+            try {
+                String token = authorizationHeader.substring("Bearer ".length());
+                Algorithm algorithm = Algorithm.HMAC256("minha-palavra-secreta");
+                JWTVerifier verifier = JWT.require(algorithm).build();
+                DecodedJWT decodedJWT = verifier.verify(token);
+                String usuario = decodedJWT.getSubject();
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(usuario, null, null);
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                filterChain.doFilter(request, response);
+            } catch (Exception ex) {
+                log.error("Mensagem de erro de autorizacao: {}", ex.getMessage());
+                response.setHeader("error", ex.getMessage());
+                response.setStatus(HttpStatus.FORBIDDEN.value());
+            }
+        } else {
+            filterChain.doFilter(request, response);
+        }
+    }
+}
---
+++ src/main/java/diogft/AgendaMed/domain/service/UsuarioService.java
@@ -0,0 +1,47 @@
+package diogft.AgendaMed.domain.service;
+
+import diogft.AgendaMed.domain.entity.Usuario;
+import diogft.AgendaMed.domain.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+
+public class UsuarioService implements UserDetailsService {
+
+    private final UsuarioRepository repository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String usuario) throws UsernameNotFoundException {
+        Optional<Usuario> optUsuario = repository.findByUsuario(usuario);
+
+        if (optUsuario.isEmpty()) {
+            throw new UsernameNotFoundException("Usuario não econtrado");
+        }
+        Usuario user = optUsuario.get();
+        return new User(user.getUsuario(), user.getSenha(), new ArrayList<>());
+    }
+
+    public List<Usuario> getAll() {
+        return repository.findAll();
+    }
+
+    public Usuario save(Usuario usuario) {
+        usuario.setSenha(passwordEncoder.encode(usuario.getSenha()));
+        return repository.save(usuario);
+    }
+}
